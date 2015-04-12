0e8d4383c97b88df72ad741fd34ce9e678317185
Zhilei Miao <zhileimiao@dyn-129-236-235-60.dyn.columbia.edu>
2015-04-12 16:04:47 -0400

    add instat module

--- instatlex.py
+++ instatlex.py
@@ -7,7 +7,23 @@ class InstatLexer(object):
         'print': 'PRINT',
         'show': 'SHOW',
         'search': 'SEARCH',
-        'string': 'STRINGF'
+        'string': 'STRINGF',
+        'from': 'FROM',
+        'to': 'TO',
+        'if': 'IF',
+        'else': 'ELSE',
+        'elif': 'ELIF',
+        'while': 'WHILE',
+        'for': 'FOR',
+        'in': 'IN',
+        'length': 'LENGTH',
+        'function': 'FUNCTION',
+        'barchart': 'BARCHART',
+        'piechart': 'PIECHART',
+        'login': 'LOGIN',
+        'logout': 'LOGOUT',
+        'main': 'MAIN',
+        'set': 'SET'
     }
 
     unreserved_tokens = [
@@ -19,7 +35,10 @@ class InstatLexer(object):
         'HASHTAG',
         'ID',
         'SEMICOLON',
-        'NEWLINE'
+        'NEWLINE',
+        'USER',
+        'LOCATION',
+        'DATE'
     ]
 
     tokens = unreserved_tokens + list(reserved.values())
@@ -28,6 +47,25 @@ class InstatLexer(object):
     t_STRING = r'\"([^"]|\n)*\"'
     t_HASHTAG = r'\#[a-zA-Z0-9_][a-zA-Z0-9_]*'
     t_SEMICOLON = r';'
+    t_FROM = r'from'
+    t_TO = r'to'
+    t_LOGIN = r'login'
+    t_LOGOUT = r'logout'
+    t_FOR = r'for'
+    t_WHILE = r'while'
+    t_IF = r'if'
+    t_ELSE = r'else'
+    t_ELIF = r'elif'
+    t_IN = r'in'
+    t_LENGTH = r'length'
+    t_FUNCTION = r'function'
+    t_PIECHART = r'piechart'
+    t_BARCHART = r'barchart'
+    t_MAIN = r'main'
+    t_SHOW = r'show'
+    t_SEARCH = r'search'
+    t_PRINT = r'print'
+    t_SET = r'set'
 
     def __init__(self):
         # Set up a logging object
@@ -46,6 +84,20 @@ class InstatLexer(object):
         t.type = InstatLexer.reserved.get(t.value, 'ID')    # Check for reserved words
         return t
 
+    def t_DATE(self, t):
+        r'[0-3]?[0-9]/[01]?[0-9]/[0-9]+[ ][01]?[0-9]:[0-5][0-9][ ]((AM)|(PM))'
+        return t
+
+    def t_USER(selt, t):
+        r'\@[a-zA-Z0-9_][a-zA-Z0-9_\.]*[a-zA-Z0-9_]'
+        t.value = t.value[1:]
+        return t
+
+    def t_LOCATION(self, t):
+        r'\@\('
+        t.value = None
+        return t
+
     def t_NONE(self, t):
         r'None'
         t.value = None
@@ -60,7 +112,7 @@ class InstatLexer(object):
             t.value = 0
         return t
 
-    def t_FLOAT(t):
+    def t_FLOAT(self, t):
         r'\d+\.\d+'
         try:
             t.value = float(t.value)
@@ -85,7 +137,7 @@ class InstatLexer(object):
         t.lexer.lineno += len(t.value)
         t.type = 'NEWLINE'
 
-    t_ignore = r' \t'
+    t_ignore = ' \t'
 
     def t_error(self, t):
         print("Illegal character '%s'" % t.value[0])
@@ -95,7 +147,19 @@ if __name__ == '__main__':
     instatlexer = InstatLexer()
 
     data = '''
-        // Hello world program for Instat
+        // Hello world program for Instat 
+
+        if else elif main from login logout for while in piechart barchart
+        to
+        function
+        set
+        length
+
+        @asd.asdasdasd
+        @(
+        search
+        25/08/2015 10:50 AM
+
         print "hello world";
         show #helloworld;
         '''
