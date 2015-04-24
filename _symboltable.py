dab4e9dfbb6f2f88a096acfb9e0c5a1a48ac1092
Enze Li <enzeli0714@gmail.com>
2015-04-24 16:39:45 -0400

    added simple symbol table

---
+++ _symboltable.py
@@ -0,0 +1,20 @@
+
+# symbol table
+
+tablestack = [{},]
+
+def add_entry(symbol, attributes):
+    global tablestack
+    tablestack[-1][symbol] = attributes
+
+def enter_scope():
+    global tablestack
+    tablestack.append({})
+
+def leave_scope():
+    global tablestack
+    tablestack.pop()
+
+def search_symbol(symbol):
+    global tablestack
+    return tablestack[-1].get(symbol, None)
